7adbf3a039781795684055edcdd357061bbf7b35
Morvader <franciscomoreno@gmail.com>
2018-07-25 18:02:09 +0200

    Added more test

--- api/controllers/filmsController.js
+++ api/controllers/filmsController.js
@@ -3,22 +3,26 @@ const FilmRepository = require('../models/filmsRepository');
 
 const filmRepository = new FilmRepository();
 
-exports.list_all_films = function(req, res) {
+exports.list_all_films = function (req, res) {
     const response = {
         films: filmRepository.fetchAll()
     };
-    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    });
     res.end(JSON.stringify(response));
-  };
+};
 
-exports.create_a_film = function(req, res){
+exports.create_a_film = function (req, res) {
 
     var new_film = Film.fromJson(req.body);
 
     var filmExist = filmRepository.getById(new_film.id);
 
-    if(filmExist != undefined){
-        res.writeHead(500, {'Content-Type': 'application/json'});
+    if (filmExist != undefined) {
+        res.writeHead(500, {
+            'Content-Type': 'application/json'
+        });
         res.end(JSON.stringify("Duplicate ID"));
     }
 
@@ -32,30 +36,40 @@ exports.delete_a_film = (req, res) => {
     const response = {
         cod: filmRepository.delete(id)
     }
-    if(response.cod === -1){
-        res.writeHead(404, {'Content-Type': 'application/json'});
+    if (response.cod === -1) {
+        res.writeHead(404, {
+            'Content-Type': 'application/json'
+        });
         res.end(JSON.stringify("ID Not found"));
     }
     res.end(JSON.stringify("Film Deleted"));
 };
 
-exports.list_init_data = function(req, res){
+exports.list_init_data = function (req, res) {
     filmRepository.clear();
 
-    filmRepository.insert(new Film(1,"Star Wars", "Space", 1980));
-    filmRepository.insert(new Film(2,"Superman", "Comic", 1986));
-    filmRepository.insert(new Film(3,"Indiana Jones", "Adventures", 1985));
+    filmRepository.insert(new Film(1, "Star Wars", "Space", 1980));
+    filmRepository.insert(new Film(2, "Superman", "Comic", 1986));
+    filmRepository.insert(new Film(3, "Indiana Jones", "Adventures", 1985));
 
     res.end();
 };
 
-exports.read_a_film = function(req, res){
+exports.read_a_film = function (req, res) {
     var id = req.params.filmId;
 
-    const response = {
+    response = {
         film: filmRepository.getById(id)
     };
+    var returnCode = 200;
 
-    res.writeHead(200, {'Content-Type': 'application/json'});
+    if (typeof response.film === "undefined") {
+        returnCode = 404;
+        response = "Film not found";
+    }
+
+    res.writeHead(returnCode, {
+        'Content-Type': 'application/json'
+    });
     res.end(JSON.stringify(response));
 };
--- api/test/filmsController.test.js
+++ api/test/filmsController.test.js
@@ -4,8 +4,8 @@ const sinon = require("sinon");
 
 var filmList = require('../controllers/filmsController');
 
-describe('Controller Test', function () {
-    beforeEach(()=>{
+describe('Films Controller Test', function () {
+    beforeEach(() => {
         let reqInit = {}
         let resInit = {
             end: sinon.spy(),
@@ -13,19 +13,100 @@ describe('Controller Test', function () {
             writeHead: sinon.spy()
         }
         //Init data
-        filmList.list_init_data(reqInit,resInit);
+        filmList.list_init_data(reqInit, resInit);
     });
-    it('Get all films', function () {
+    it('Get all films should return intt films', function () {
         let req = {}
         let res = {
             end: sinon.spy(),
-            send: sinon.spy(),
             writeHead: sinon.spy()
         }
-        
+
         filmList.list_all_films(req, res);
         var films = JSON.parse(res.end.firstCall.args[0]);
         expect(films.films[0].id).to.equal(1);
+
+    });
+
+    it('Get one film should return data of the specific film', function () {
+
+        let req = {
+            params: {
+                filmId: 1
+            }
+        }
+        let res = {
+            end: sinon.spy(),
+            writeHead: sinon.spy()
+        }
+
+        filmList.read_a_film(req, res);
+        var films = JSON.parse(res.end.firstCall.args[0]);
+        expect(films.film.id).to.equal(1);
+
+    });
+    it('Get non existing film should return 404', function () {
+
+        let req = {
+            //Id no existe
+            params: {
+                filmId: 10
+            }
+        }
+        let res = {
+            end: sinon.spy(),
+            writeHead: sinon.spy()
+        }
+
+        filmList.read_a_film(req, res);
+        var returnCode = JSON.parse(res.writeHead.firstCall.args[0]);
+        expect(returnCode).to.equal(404);
+
+    });
+
+    it('Create a new Film should add the film to repository', function () {
+        var newFilmData = {
+            id: 10,
+            Name: "New Test Film",
+            Description: "New Film Description",
+            Year: 2020
+        }
+        let req = {
+            body: newFilmData
+        }
+        let res = {
+            end: sinon.spy(),
+            writeHead: sinon.spy()
+        }
+
+        filmList.create_a_film(req, res);
         
+        var returnData = JSON.parse(res.end.firstCall.args[0]);
+        expect(returnData).to.deep.equals(newFilmData);
+
+    });
+
+    it('Create same ID Film should return an error', function () {
+        var newFilmData = {
+            id: 1,
+            Name: "New Test Film",
+            Description: "New Film Description",
+            Year: 2020
+        }
+        let req = {
+            body: newFilmData
+        }
+        let res = {
+            end: sinon.spy(),
+            writeHead: sinon.spy()
+        }
+
+        filmList.create_a_film(req, res);
+        
+        var returnCode = JSON.parse(res.writeHead.firstCall.args[0]);
+        var returnResponse = JSON.parse(res.end.firstCall.args[0]);
+        expect(returnCode).to.equals(500);
+        expect(returnResponse).to.be.equal("Duplicate ID");
+
     });
 });
